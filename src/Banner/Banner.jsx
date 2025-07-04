dea665acee2e81d66caca4203747c5addca08079
sirmufasas <jonathankabango08@gmail.com>
2025-07-04 10:11:29 +0200

    Made the wire frames

---
+++ src/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Banner.css'
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-content'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Flight/Flight.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Flight.css'
+
+const Flight = () => {
+  return (
+    <div className='flight'>
+      <div className='flight-content'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Flight
