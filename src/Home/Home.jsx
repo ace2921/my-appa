4f6fb35d46e6f8cc6e4c0ff8a486c16663ae53d1
christophermossi <mossichris18@iCloud.com>
2025-07-11 10:20:13 +0200

    Your commit message

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,10 +4,29 @@ import './Home.css'
 const Home = () => {
   return (
     <div>
-      <div className='navbar'>
+      <div className='navbar-section'>
+          
+  
+    <ul className="navbar">
+      <li><a className="active" href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+      <span role="img" aria-label="airplane" className="thefooter-logo">✈️</span>
+      <span className="thefooter-title">SkyHigh Airlines</span>
+
+    </ul>
+  
+
       </div>
     </div>
+
+
+
+
+
+    
   )
 }
 
-export default Home
+export default Home
